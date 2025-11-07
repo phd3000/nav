4dd485923c621b461393d2bcfa7824f7faa8ad10
phd3000 <160401811+phd3000@users.noreply.github.com>
2025-11-07 09:40:23 +0000

    Deploying to gh-pages from @ phd3000/nav@bd0e8676d6fcb08b764f64f26834353a05d07eeb 🚀

---
+++ chunk-PN3HM65U.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-4VL2I6FQ.js";import{qb as r,zb as e}from"./chunk-B3MQN7KR.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
